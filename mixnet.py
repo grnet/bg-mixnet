469964598d9306e624616f9ae9d07ed33becbb7c
Marios Fragkoulis <mfg@grnet.gr>
2018-04-12 16:26:48 +0300

    Generate votes using Zeus (random election)

--- mixnet.py
+++ mixnet.py
@@ -2,9 +2,9 @@ import pybgmix
 import sys
 from celery import Celery
 from celery.utils.log import get_task_logger
+from Crypto import Random
 
 logger = get_task_logger(__name__)
-sys.stdout = logger.info
 
 mixnet_app = Celery('mixnet',
                     backend='rpc://',
@@ -14,7 +14,22 @@ ciphers_file = "py_ciphers.txt"
 
 @mixnet_app.task
 def generate_ciphers():
-    pybgmix.bg_generate_ciphers(ciphers_file)
+    try:
+        from zeus_legacy.zeus.core import ZeusCoreElection
+        # Does zeus fork() within mk_random()? Without the following
+        # statement the Crypto module raises an AssertionError
+        # suggesting that Random.atfork() is required.
+        Random.atfork()
+
+        election = ZeusCoreElection.mk_random(
+                            nr_votes        =   10000,
+                            stage           =   "VOTING",
+                            )
+        mix, counted_list = election.extract_votes_for_mixing()
+        print "mix: %s" % str(mix)
+        print "counted_list: %s" % str(counted_list)
+    except ImportError:
+        pybgmix.bg_generate_ciphers(ciphers_file)
 
 @mixnet_app.task
 def mix():

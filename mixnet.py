c966755b3e4b6b3b3f1261ad5a8255d741ac2403
Marios Fragkoulis <mfg@grnet.gr>
2018-04-30 14:10:53 +0300

    Parallelise cipher generation; reuse cipher file if exists

--- mixnet.py
+++ mixnet.py
@@ -1,6 +1,4 @@
 import pybgmix
-import sys
-import json
 from celery import Celery
 from celery.utils.log import get_task_logger
 
@@ -10,31 +8,18 @@ mixnet_app = Celery('mixnet',
                     backend='rpc://',
                     broker='pyamqp://guest@localhost//')
 
-ciphers_file = "ciphers.json"
 
 @mixnet_app.task
-def generate_ciphers(dim_m, dim_n):
-    try:
-        from zeus_legacy.zeus.core import ZeusCoreElection
-
-        # Does zeus fork() within mk_random()? Without the following
-        # statement the Crypto module raises an AssertionError
-        # suggesting that Random.atfork() is required.
-        from Crypto import Random
-        Random.atfork()
-
-        election = ZeusCoreElection.mk_random(
-                            nr_votes        =   dim_m * dim_n,
-                            stage           =   "VOTING",
-                            )
-        mix, counted_list = election.extract_votes_for_mixing()
-	with open(ciphers_file, 'w') as f:
-		json.dump(mix, f)
-        # print "mix: %s" % str(mix)
-        # print "counted_list: %s" % str(counted_list)
-    except ImportError:
-        pybgmix.bg_generate_ciphers(ciphers_file, dim_m, dim_n)
+def encrypt_cipher(i, modulus, generator, order, public_key):
+    from zeus_legacy.zeus.core import encrypt
+    if i % 1000 == 0:
+        logger.info("Ciphers encrypted: %d", i)
+    return encrypt(i, modulus, generator, order, public_key)[0:2]
 
 @mixnet_app.task
-def mix(dim_m, dim_n):
-    pybgmix.bg_mix(ciphers_file, dim_m, dim_n)
+def generate_ciphers(ciphers_file, dim_m, dim_n):
+    return pybgmix.bg_generate_ciphers(ciphers_file, dim_m, dim_n)
+
+@mixnet_app.task
+def mix(ciphers_file, dim_m, dim_n):
+    return pybgmix.bg_mix(ciphers_file, dim_m, dim_n)
--- run_mixnet.py
+++ run_mixnet.py
@@ -1,5 +1,9 @@
-from mixnet import mix, generate_ciphers
+from mixnet import mix, generate_ciphers, encrypt_cipher
 import sys
+import os
+import json
+import multiprocessing
+
 
 try:
     dim_m = long(sys.argv[1])
@@ -11,8 +15,47 @@ try:
 except IndexError:
     dim_n = 2
 
-r = generate_ciphers.delay(dim_m, dim_n)
-r.get()
-if r.successful():
-    r = mix.delay(dim_m, dim_n)
-    r.get()
+try:
+    ciphers_file = sys.argv[3]
+except IndexError:
+    ciphers_file = 'ciphers.json'
+
+CHUNKS = multiprocessing.cpu_count()
+ciphers_file_exists = False
+
+try:
+    from zeus_legacy.zeus.core import ZeusCoreElection
+
+    if not os.path.isfile(ciphers_file):
+        election = ZeusCoreElection()
+        crypto = election.do_get_cryptosystem()
+        modulus, generator, order = crypto
+        _, public_key, _, _, _ = election.create_zeus_key()
+
+        result = encrypt_cipher.chunks([(i, modulus, generator,
+            order, public_key) for i in xrange(dim_m * dim_n)], CHUNKS)
+        result = result.apply_async()
+        cipher_chunks = result.get()
+        ciphers = [cipher for chunk in cipher_chunks
+            for cipher in chunk]
+        # print 'original_ciphers: %s' % str(ciphers)
+        crypto_ciphers = {'modulus': modulus,
+               'generator': generator,
+               'order': order,
+               'public': public_key,
+               'original_ciphers': ciphers,
+               'mixed_ciphers': ciphers}
+        with open(ciphers_file, 'w') as f:
+            json.dump(crypto_ciphers, f)
+    else:
+        ciphers_file_exists = True
+except ImportError:
+    result = generate_ciphers.delay(ciphers_file, dim_m, dim_n)
+    result.get()
+
+if ciphers_file_exists or result.successful():
+    result = mix.delay(ciphers_file, dim_m, dim_n)
+    mix_success = result.get()
+    if not mix_success:
+        print "Shuffle failed. Check log file."
+        sys.exit(1)

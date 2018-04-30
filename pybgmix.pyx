5f7e6edd319b113e0331c0c2dcd0fd90fafbbf27
Marios Fragkoulis <mfg@grnet.gr>
2018-04-30 11:56:56 +0300

    Pass on return value of C++ BG mixnet to Python

--- pybgmix.pyx
+++ pybgmix.pyx
@@ -1,7 +1,7 @@
 cimport pybgmix
 
 def bg_generate_ciphers(ciphers_file, dim_m, dim_n):
-    pybgmix.generate_ciphers(ciphers_file, dim_m, dim_n)
+    return pybgmix.generate_ciphers(ciphers_file, dim_m, dim_n)
 
 def bg_mix(ciphers_file, dim_m, dim_n):
-    pybgmix.mix(ciphers_file, dim_m, dim_n)
+    return pybgmix.mix(ciphers_file, dim_m, dim_n)
